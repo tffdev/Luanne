cc00ef9577f18760cb1bb3045b535e1f14f69beb
Daniel Brier <Daniel Brier>
2018-02-22 18:06:31 +0000

    Deleted luanne debug copy, changed conf

--- luanne-debug-2/config.lua
+++ luanne-debug-2/config.lua
@@ -13,7 +13,7 @@ exp_out = {{0,0},{0,1},{0,0},{0,1}}
 -- Parameters
 math.randomseed(123)
 STRUCTURE 				= { 2, 10, 10, 2}
-learning_rate 			= 2
+learning_rate 			= 0.1
 momentum_multiplier		= 0.5
 iterations_per_frame 	= 50
 updates_per_sec 		= 60
