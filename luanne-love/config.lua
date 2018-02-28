feddcf1b92d43bf058a7ace121b538c0c5a4565c
Daniel Brier <Daniel Brier>
2018-02-28 09:34:01 +0000

    new word doc

--- luanne-love/config.lua
+++ luanne-love/config.lua
@@ -13,11 +13,8 @@ exp_out = {{0,0},{0,1},{0,0},{0,1}}
 -- Parameters
 math.randomseed(123)
 STRUCTURE 				= { 2, 10, 10, 2}
-learning_rate 			= 0.1
-<<<<<<< HEAD
-momentum_multiplier		= 0.5
-=======
+learning_rate 			= 2
+momentum_multiplier		= 0.9
 momentum_multiplier		= 0.1
->>>>>>> 0eb500bb6447ad9af40f4d82a8e180e2cb22e947
 iterations_per_frame 	= 50
 updates_per_sec 		= 60
