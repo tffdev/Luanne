c5d80240ec7aceb1bbc8666a3a9732b853b96a2e
Daniel Taffe <danielbri3r@gmail.com>
2021-03-29 15:14:36 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,21 +1,3 @@
---[[
-
-Micro Neural Net Framework [For Project 'NLPS', Daniel Brier]
---------------------------------
-
-This file contains an example of machine learning the function
-XOR. XOR is popular as it can only be performed on a network
-with functional hidden layers.
-
-What we've done here is create a new network with an input vector 
-of length 2, an output vector of length 1,
-and a hidden layer of length 3.
-
-Our learning rate (the velocity of a synapse value "kick") 
-is 0.1, and Momentum is 0.03, both of which are sufficient for this small demo
-but usually, the values would be MUCH smaller than this.
-]]--
-
 local luanne = require("luanne")
 local nn = luanne:new_network( { 2, 3, 1 } , 0.1, 0.03)
 	
@@ -32,9 +14,6 @@ for _ = 1, 20 do
 	print(string.format("Average error: %0.5f", err))
 end
 
-
--- nn:forward takes an input vector, performs a pass on your network
--- and returns an output vector! This is your generated function.
 print(string.format(
 [[--------------------
 Results of XOR Test!
